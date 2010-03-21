ba29d497fd4292bdd850bb72b6db923ab0e3f24e
Jared Grippe <jared@deadlyicon.com>
2010-03-21 01:03:35 -0700

    added event emitter lib

---
+++ event_emitter.js
@@ -0,0 +1,19 @@
+function EventEmitter(){
+  this._listeners = {};
+}
+EventEmitter.prototype.addListener = function addListener(type, handler){
+  this._listeners[type] || (this._listeners[type] = []);
+  this._listeners[type].push(handler);
+  return this;
+};
+EventEmitter.prototype.emit = function emit(type, data){
+  var handlers = this._listeners[type];
+
+  if (handlers) for (var i=0; i < handlers.length; i++)
+    handlers[i].call(this, this, data);
+
+  return this;
+};
+
+if (typeof exports !== "undefined")
+  exports.EventEmitter = EventEmitter;
